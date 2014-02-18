87be90a9d77cb5718b0d32480f18d914a302e72a
Tarwine <tarwine0406@gmail.com>
2014-02-18 14:14:03 -0500

    Reworking borders and organization

---
+++ Assets/Scripts/Border.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Border : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerStay(Collider other){
+		if(other.gameObject.tag == "Player"){
+			other.transform.parent.rigidbody.AddForce(this.transform.up * 10.0f);
+
+			//other.rigidbody.AddForce(this.transform.forward * 10.0f);
+		}
+	}
+}
---
+++ Assets/Scripts/Debris.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Debris : MonoBehaviour {
+	public GameObject explosionEffect;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.tag == "Player"){
+			GameObject.Instantiate(explosionEffect, this.transform.position, this.transform.rotation);
+			GameObject.Destroy(this.gameObject);
+		}
+	}
+}
