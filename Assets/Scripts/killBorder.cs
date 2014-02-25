29234cff1842f66771a2f2c6da53fa4562914d68
Tarwine <tarwine0406@gmail.com>
2014-02-25 13:18:36 -0500

    Added kill borders on edge of level

---
+++ Assets/Scripts/killBorder.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class killBorder : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider other){
+		if(other.tag == "Player"){
+			Application.LoadLevel(Application.loadedLevel); 
+		}
+	}
+}
