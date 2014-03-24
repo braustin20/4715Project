b64d3ed922322b829284bad5202ce36219168e39
Tarwine <tarwine0406@gmail.com>
2014-03-23 22:57:40 -0400

    Laser charge trigger added to lvl1

---
+++ Assets/Scripts/LaserChargeTrigger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserChargeTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.tag == "Ship"){
+			Debug.Log("Laser Charging");
+			GameObject.Find("ChargeUpParticle").GetComponent<ParticleEmitter>().emit = true;
+			GameObject.Find("ChargeUpParticle").GetComponent<Light>().enabled = true;
+		}
+	}
+}
---
+++ Assets/Scripts/LaserGrow.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserGrow : MonoBehaviour {
+	public float finalMaxSize = 50.0f;
+	public float finalMaxAmt = 100.0f;
+
+	private bool finalReached = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!finalReached){
+			if(particleEmitter.maxSize < 50.0f  && particleEmitter.emit){
+				this.particleEmitter.maxSize += 0.01f;
+				this.particleEmitter.minSize += 0.01f;
+				this.particleEmitter.maxEmission += 0.01f;
+				this.particleEmitter.minEmission += 0.01f;
+			}
+			else if(particleEmitter.maxSize >= 50.0f  && particleEmitter.emit){
+				finalReached = true;
+			}
+		}
+	}
+}
