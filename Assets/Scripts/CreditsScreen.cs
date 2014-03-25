a3b0d5aab727d0f831efa09e533b945c23fd4248
Tarwine <tarwine0406@gmail.com>
2014-03-24 23:09:05 -0400

    Made credits screen return to menu

---
+++ Assets/Scripts/CreditsScreen.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void OnGUI() {
+		if(Event.current.type == EventType.KeyDown || Event.current.type == EventType.MouseDown) {
+			Application.LoadLevel(0);
+		}
+	}
+}
