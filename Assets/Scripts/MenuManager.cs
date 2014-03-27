b5cc4b9d1496096b6eda9f0b4ebd2518deb2ecb6
Tarwine <tarwine0406@gmail.com>
2014-03-27 01:14:48 -0400

    Added menu manager to ensure mouse control

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Screen.showCursor = true;
+		Screen.lockCursor = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
