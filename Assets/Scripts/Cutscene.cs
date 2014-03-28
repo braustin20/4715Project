3f111c929eba77c0095c8fc0f8c0f809fe5dc3ea
Tarwine <tarwine0406@gmail.com>
2014-03-28 14:19:14 -0400

    Added cutscene test

---
+++ Assets/Scripts/Cutscene.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cutscene : MonoBehaviour {
+	private float timer = 0.0f;
+	private float duration;
+
+	// Use this for initialization
+	void Start () {
+		MovieTexture movie = renderer.material.mainTexture as MovieTexture;
+		movie.Play();
+		duration = movie.duration;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if(timer >= duration){
+			Application.LoadLevel(Application.loadedLevel + 1);
+		}
+	}
+}
