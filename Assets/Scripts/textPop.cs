c63c624ed13f0d3124d1b4b9a3ec195142c0c90b
James <James@Macintosh.local>
2014-04-08 17:14:11 -0400

    level 3
    added a couple lights, added click text when you get near escape pod.
    Also made cutscene skip text smaller.

---
+++ Assets/Scripts/textPop.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class textPop : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		foreach (Renderer r in this.GetComponentsInChildren(typeof(Renderer)))
+			
+		{
+			r.enabled = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		foreach (Renderer r in this.GetComponentsInChildren(typeof(Renderer)))
+			
+		{
+			r.enabled = true;
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		foreach (Renderer r in this.GetComponentsInChildren(typeof(Renderer)))
+			
+		{
+			r.enabled = false;
+		}
+	}
+}
