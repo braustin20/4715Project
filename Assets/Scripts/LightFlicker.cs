945ad7643a439c7cc8eb7db4eb5c22dbbf48ac82
Tarwine <tarwine0406@gmail.com>
2014-04-09 23:37:44 -0400

    Added flickering lights to level 3

---
+++ Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightFlicker : MonoBehaviour {
+	private Light[] lightList;
+
+	private float duration;
+	private float timer;
+
+	// Use this for initialization
+	void Start () {
+		lightList = this.GetComponentsInChildren<Light>();
+		duration = Random.Range(0.0f, 1.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		timer += Time.deltaTime;
+		if(timer >= duration){
+			foreach(Light light in lightList){
+				if(light.enabled == false){
+					light.enabled = true;
+				}
+				else{
+					light.enabled = false;
+				}
+			}
+			duration = Random.Range(0.0f, 1.0f);
+			timer = 0.0f;
+		}
+	}
+}
